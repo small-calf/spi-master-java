a9d857884402392447bfd58daa364ae5328994dd
wangyulong <1833415795@qq.com>
2023-06-16 18:06:32 +0800

    spi接口定义

---
+++ spi-business-weixinpay/src/main/java/com/calf/spibusinessweixinpay/impl/WeixinPayImpl.java
@@ -0,0 +1,15 @@
+package com.calf.spibusinessweixinpay.impl;
+
+import com.calf.spiinterface.plugin.PayPlugin;
+
+/**
+ * @author: wangyulong
+ * @date: 2023/6/16 18:02
+ * @description
+ */
+public class WeixinPayImpl implements PayPlugin {
+    @Override
+    public void pay() {
+
+    }
+}
---
+++ spi-interface/src/main/java/com/calf/spiinterface/plugin/PayPlugin.java
@@ -0,0 +1,10 @@
+package com.calf.spiinterface.plugin;
+
+/**
+ * @author: wangyulong
+ * @date: 2023/6/16 17:49
+ * @description spi接口，提供给其他外部业务实现
+ */
+public interface PayPlugin {
+    void pay();
+}
